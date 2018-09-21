4fb69751a7bc23cfc3bdbebf54ee1009c5686d1a
张雪培 <xuepei.zhang@phicomm.com>
2018-09-21 09:18:23 +0800

    用户列表添加搜索功能

--- application/admin/controller/Manager.php
+++ application/admin/controller/Manager.php
@@ -30,8 +30,54 @@ class Manager extends Adminbase
      */
     public function index()
     {
-        $User = Db::name("admin")->where('status','>',0)->order(array('roleid' => 'ASC'))->select();
-        $this->assign("Userlist", $User);
+        $page = 1;  //请求页
+        $pagesize = 10;  //页码大小
+        $limit = 0; //记录起始
+
+        if(isset($_POST['page']) && (int)$_POST['page'] && isset($_POST['fenye']) && $_POST['fenye']==1){   //当时fenye请求时page参数才有效
+            $page = (int)$_POST['page'];
+            $limit = ($page-1)*$pagesize;
+        }
+
+        //响应查询请求
+        $searchwhere['r'] = -1;
+        $searchwhere['u'] = $searchwhere['n'] = '';
+        $where = array();
+        $where[] = ['status','>',0];
+        if(isset($_POST['roleid']) && $_POST['roleid'] != -1){
+            $where[] = ['roleid','eq',(int)$_POST['roleid']];
+            $searchwhere['r'] = $_POST['roleid'];
+        }
+        if(isset($_POST['username']) && $_POST['username']){
+            $where[] = ['username','like','%'.$_POST['username'].'%'];
+            $searchwhere['u'] = $_POST['username'];
+        }
+        if(isset($_POST['nickname']) && $_POST['nickname']){
+            $where[] = ['nickname','like','%'.$_POST['nickname'].'%'];
+            $searchwhere['n'] = $_POST['nickname'];
+        }
+
+        //角色列表
+        $rolelist = array(
+            ['id'=>-1, 'title'=>'全部角色']
+        );
+        $role = Db::name("auth_group")->where('status','eq','1')->select();
+        if(!empty($role)){
+            foreach ($role as $item) {
+                $rolelist[] = ['id'=>$item['id'], 'title'=>$item['title']];
+            }
+        }
+
+        $User = Db::name("admin")->where($where)->order(array('roleid' => 'ASC'))->limit($limit)->select();
+        $count = Db::name("admin")->where($where)->count();
+
+        $this->assign('count', $count); //总条数
+        $this->assign('pagesize', $pagesize); //页码大小
+        $this->assign('page', $page); //当前页
+
+        $this->assign("Userlist", $User);   //用户列表
+        $this->assign('rolelist', $rolelist);  //角色列表
+        $this->assign('searchwhere', $searchwhere); //搜索条件
         return $this->fetch();
     }
 
